64be71c76d1127a5843f05a90b6ce430375e3fa9
ChanifRusydi <chanif.rusydi@gmail.com>
2022-05-18 07:34:41 +0700

    rename writing_data.py and working on getting_data.py

--- getting_data.py
+++ getting_data.py
@@ -1,12 +1,9 @@
-\
 import json
-def getting_data():
+def getting_data(index,source_file):
    train_file=open('data/squad/train-v2.0.json','r')
    train_data=json.load(train_file)
    train_file.close()
-   first_text=train_data['data'][0]['paragraphs'][0]['qas'][0]['answers'][0]['text']
-   print((train_data['data'][0]['paragraphs'][0]['qas'][0]['answers'][0]['text']))
-   print((train_data['data'][0]['paragraphs'][0]['qas'][0]['question']))
+   text=train_data['data'][index]['paragraphs'][0]['context']
    return first_text
 
 getting_data()
